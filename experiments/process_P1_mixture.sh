050a8a92960abd3b6a00f58b3cb353906b7d78fc
Joe Wandy <joewandy@gmail.com>
2014-12-07 23:46:20 +0000

    tidy up

--- experiments/process_P1_mixture.sh
+++ experiments/process_P1_mixture.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-METHOD=$1
-FRAC=$2
-ALIGNMENT_MZTOL=$3
-ALIGNMENT_RTTOL=$4
-GROUPING_RTTOL=$5
-ALPHA=$6
+FRAC=$1
+ALIGNMENT_MZTOL=$2
+ALIGNMENT_RTTOL=$3
+GROUPING_RTTOL=$4
+ALPHA=$5
 
-java -ea -jar ../alignment.jar -d ../input_data/P1/${FRAC} -gt ../input_data/P1/ground_truth/ground_truth_${FRAC}.txt -method ${METHOD} -alignmentMzTol ${ALIGNMENT_MZTOL} -alignmentRtTol ${ALIGNMENT_RTTOL} -useGroup true -groupingMethod posterior -groupingRtWindow ${GROUPING_RTTOL} -alpha ${ALPHA}
+java -ea -jar ../alignment.jar -d ../input_data/P1/${FRAC} -gt ../input_data/P1/ground_truth/ground_truth_${FRAC}.txt -method MW -alignmentMzTol ${ALIGNMENT_MZTOL} -alignmentRtTol ${ALIGNMENT_RTTOL} -useGroup true -groupingMethod posterior -groupingRtWindow ${GROUPING_RTTOL} -alpha ${ALPHA}
